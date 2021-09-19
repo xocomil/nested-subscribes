d402e8b1cee107f79fcbb78f396d9ca58d86a290
xocomil <xocomil@gmail.com>
2021-09-19 10:59:11 -0600

    initial commit

---
+++ apps/nested-subscribes/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'nested-subscribes',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/nested-subscribes',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
