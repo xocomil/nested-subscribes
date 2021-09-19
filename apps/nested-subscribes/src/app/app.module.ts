4629abe00b5562436adecf94a3bbc1e2f7cd15d7
xocomil <xocomil@gmail.com>
2021-09-19 12:21:07 -0600

    add angular material

--- apps/nested-subscribes/src/app/app.module.ts
+++ apps/nested-subscribes/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [AppComponent, PokemonListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/nested-subscribes/src/app/material.module.ts
@@ -0,0 +1,31 @@
+import { A11yModule } from '@angular/cdk/a11y';
+import { BidiModule } from '@angular/cdk/bidi';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ObserversModule } from '@angular/cdk/observers';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PlatformModule } from '@angular/cdk/platform';
+import { PortalModule } from '@angular/cdk/portal';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  exports: [
+    // CDK
+    A11yModule,
+    BidiModule,
+    ObserversModule,
+    OverlayModule,
+    PlatformModule,
+    PortalModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    // Material
+    MatTableModule,
+  ],
+})
+export class MaterialModule {}
