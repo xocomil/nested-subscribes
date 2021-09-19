635552261c15c682d94fd40ed4deb1499789a0ad
xocomil <xocomil@gmail.com>
2021-09-19 15:55:35 -0600

    add missing module

--- apps/nested-subscribes/src/app/app.module.ts
+++ apps/nested-subscribes/src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { NestedSubscribeComponent } from './components/nested-subscribe/nested-subscribe.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { MaterialModule } from './material.module';
-import { NestedSubscribeComponent } from './components/nested-subscribe/nested-subscribe.component';
 
 @NgModule({
   declarations: [AppComponent, PokemonListComponent, NestedSubscribeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
