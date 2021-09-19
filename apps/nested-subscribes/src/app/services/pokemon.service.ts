ded90cda42450ac483f2eb86d388b4d1399e3b92
xocomil <xocomil@gmail.com>
2021-09-19 14:36:57 -0600

    add methods to get Pokemon from PokemonService

--- apps/nested-subscribes/src/app/services/pokemon.service.ts
+++ apps/nested-subscribes/src/app/services/pokemon.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pokemon } from '../models/pokemon';
+import { PokemonSearchResults } from '../models/pokemon-search-result';
+
+const POKE_API_URL = 'https://pokeapi.co/api/v2/' as const;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  constructor(private readonly _httpClient: HttpClient) {}
 
-  constructor() { }
+  getPokemon(): Observable<PokemonSearchResults> {
+    return this._httpClient.get<PokemonSearchResults>(
+      `${POKE_API_URL}pokemon/?limit=10`
+    );
+  }
+
+  getPokemonDetails(name: string): Observable<Pokemon> {
+    return this._httpClient.get<Pokemon>(`${POKE_API_URL}pokemon/${name}/`);
+  }
 }
