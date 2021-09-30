8994c4eda1ebffee645d0f9e5fb7edf6268c0059
xocomil <xocomil@gmail.com>
2021-09-30 15:05:33 -0600

    component with mergeMap() works

--- apps/nested-subscribes/src/app/components/merge-map-sorted/merge-map-sorted.component.ts
+++ apps/nested-subscribes/src/app/components/merge-map-sorted/merge-map-sorted.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { finalize, map, mergeMap, toArray } from 'rxjs/operators';
+import { Pokemon } from '../../models/pokemon';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'nested-merge-map-sorted',
   templateUrl: './merge-map-sorted.component.html',
-  styleUrls: ['./merge-map-sorted.component.scss']
+  styleUrls: ['./merge-map-sorted.component.scss'],
 })
-export class MergeMapSortedComponent implements OnInit {
+export class MergeMapSortedComponent {
+  pokemon: Pokemon[] = [];
+  isLoading = false;
 
-  constructor() { }
+  constructor(private readonly _pokemonService: PokemonService) {}
 
-  ngOnInit(): void {
+  useMergeMap(): void {
+    if (this.isLoading) {
+      return;
+    }
+
+    this.isLoading = true;
+
+    const withConcatMap$ = this._pokemonService.getPokemon().pipe(
+      map((response) => response.results),
+      mergeMap((result) => result),
+      mergeMap((result) => this._pokemonService.getPokemonDetails(result.name))
+    );
+
+    this.getData(withConcatMap$);
   }
 
+  private getData(pokemon$: Observable<Pokemon>): void {
+    pokemon$
+      .pipe(
+        toArray(),
+        finalize(() => {
+          this.isLoading = false;
+        })
+      )
+      .subscribe((resultPokemon) => {
+        this.pokemon = resultPokemon;
+      });
+  }
 }
