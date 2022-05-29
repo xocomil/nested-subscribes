e76cd1352e820addfbfc70493a0cec4778ab076a
xocomil <xocomil@gmail.com>
2022-05-29 12:45:55 -0600

    added pinning tests

--- apps/nested-subscribes/src/app/app.component.spec.ts
+++ apps/nested-subscribes/src/app/app.component.spec.ts
@@ -1,31 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import { createComponentFactory } from '@ngneat/spectator/jest';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent],
-    }).compileComponents();
-  });
+const createComponent = createComponentFactory({ component: AppComponent });
 
+describe('AppComponent', () => {
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const spectator = createComponent();
+
+    expect(spectator).toBeTruthy();
   });
 
   it(`should have as title 'nested-subscribes'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('nested-subscribes');
-  });
+    const spectator = createComponent();
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to nested-subscribes!'
-    );
+    expect(spectator.component.title).toEqual('nested-subscribes');
   });
 });
