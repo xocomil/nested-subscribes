74ea6e3250d577bc6b25f1e0e45f723f9595597c
xocomil <xocomil@gmail.com>
2022-05-30 15:45:53 -0600

    nx migrate latest

--- apps/nested-subscribes/jest.config.ts
+++ apps/nested-subscribes/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'nested-subscribes',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
@@ -11,8 +12,9 @@ module.exports = {
   coverageDirectory: '../../coverage/apps/nested-subscribes',
   coverageReporters: ['clover', 'html', 'lcov'],
   transform: {
-    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
